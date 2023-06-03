2baf51636bb77a468d29d49b48b74d501a705502
Alim03 <ali.mohamadkhani03@gmail.com>
2023-06-04 02:15:48 +0330

    Initial

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace ChristianBeauty.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
